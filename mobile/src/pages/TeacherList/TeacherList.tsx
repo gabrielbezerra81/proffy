202ecdfbc28f99bebaffa48cb7221dc7f45fb814
Gabriel Alencar <gabriel_alencar2@hotmail.com>
2020-08-14 17:26:31 -0300

    Adicionando TimePicker no filtro de horário

--- mobile/src/pages/TeacherList/TeacherList.tsx
+++ mobile/src/pages/TeacherList/TeacherList.tsx
@@ -1,7 +1,14 @@
-import React, { useState, useCallback, useEffect } from "react";
-import { View, Text, TextInput, ScrollView } from "react-native";
+import React, { useState, useCallback } from "react";
+import {
+  View,
+  Text,
+  TextInput,
+  ScrollView,
+  TouchableWithoutFeedback,
+} from "react-native";
 import { Feather } from "@expo/vector-icons";
 import AsyncStorage from "@react-native-community/async-storage";
+import DateTimePickerModal from "react-native-modal-datetime-picker";
 
 import styles from "./styles";
 import PageHeader from "../../shared/components/PageHeader/PageHeader";
@@ -16,12 +23,36 @@ import InputSelect from "../../shared/components/InputSelect/InputSelect";
 const TeacherList = () => {
   const [subject, setSubject] = useState("Artes");
   const [week_day, setWeekDay] = useState("");
-  const [time, setTime] = useState("12:00");
+  const [time, setTime] = useState("");
 
   const [classes, setClasses] = useState<Teacher[]>([]);
   const [favorites, setFavorites] = useState<string[]>([]);
 
   const [isFiltersVisibile, setIsFiltersVisible] = useState(true);
+
+  const [date, setDate] = useState(new Date());
+  const [isTimePickerVisibile, setIsTimePickerVisibile] = useState(false);
+
+  const showDatePicker = useCallback(() => {
+    setIsTimePickerVisibile(true);
+  }, []);
+
+  const hideDatePicker = useCallback(() => {
+    setIsTimePickerVisibile(false);
+  }, []);
+
+  const handleSelectTime = useCallback((date) => {
+    hideDatePicker();
+    const selectedDate = new Date(date);
+    const selectedTime = selectedDate.getHours().toString().padStart(2, "0");
+    const selectedMinute = selectedDate
+      .getMinutes()
+      .toString()
+      .padStart(2, "0");
+    setTime(`${selectedTime}:${selectedMinute}`);
+
+    setDate(selectedDate);
+  }, []);
 
   const handleToggleFiltersVisible = useCallback(() => {
     setIsFiltersVisible((visible) => !visible);
@@ -103,13 +134,26 @@ const TeacherList = () => {
 
               <View style={styles.inputBlock}>
                 <Text style={styles.label}>Horário</Text>
-                <TextInput
-                  placeholderTextColor="#c1bccc"
-                  style={styles.input}
-                  placeholder="Qual horário"
-                  value={time}
-                  onChangeText={setTime}
-                ></TextInput>
+                <TouchableWithoutFeedback onPress={showDatePicker}>
+                  <View style={styles.input}>
+                    <Text style={{ color: time ? "black" : "#c1bccc" }}>
+                      {time ? time : "Qual horário?"}
+                    </Text>
+                  </View>
+                </TouchableWithoutFeedback>
+                <DateTimePickerModal
+                  mode="time"
+                  date={date}
+                  onCancel={hideDatePicker}
+                  onConfirm={handleSelectTime}
+                  headerTextIOS="Escolha o horário"
+                  confirmTextIOS="Confirmar"
+                  cancelTextIOS="Cancelar"
+                  isVisible={isTimePickerVisibile}
+                  display="spinner"
+                  locale="pt_BR"
+                  is24Hour
+                />
               </View>
             </View>
 
@@ -145,24 +189,5 @@ const TeacherList = () => {
     </View>
   );
 };
-
-// const DayPickerIOS = () => {
-//   <ModalSelector
-//     initValue="Qual o dia?"
-//     style={styles.input}
-//     data={[
-//       { label: "Domingo", key: "0" },
-//       { label: "Segunda-feira", key: "1" },
-//       { label: "Terça-feira", key: "2" },
-//       { label: "Quarta-feira", key: "3" },
-//       { label: "Quinta-feira", key: "4" },
-//       { label: "Sexta-feira", key: "5" },
-//       { label: "Sábado", key: "6" },
-//     ]}
-//     onChange={(option) => setWeekDay(option.label)}
-//   >
-//     <TextInput value={week_day} placeholder="Qual o dia?" editable={false} />
-//   </ModalSelector>;
-// };
 
 export default TeacherList;
